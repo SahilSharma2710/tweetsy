117797c4f1aab9b47adc96ec5cc471038e3f8631
SahilSharma2710 <s90sahil90@gmail.com>
2024-12-05 10:57:47 +0530

    app bar added

--- app/src/main/java/com/example/tweetsy/MainActivity.kt
+++ app/src/main/java/com/example/tweetsy/MainActivity.kt
@@ -2,14 +2,20 @@ package com.example.tweetsy
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
@@ -23,13 +29,30 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+
+        )
         setContent {
             TweetsyTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                  App()
+                Scaffold(
+                    topBar = {
+                            TopAppBar(title ={
+                                Text(text = "Tweetsy")
+                            },
+                                colors = TopAppBarDefaults.topAppBarColors(
+                                    containerColor = Color.Black,
+                                    titleContentColor = Color.White
+
+                                )
+                            )
+                              },
+                    modifier = Modifier.fillMaxSize()) {
+                    Box(modifier = Modifier.padding(it)){
+                        App()
+                    }
                 }
             }
         }
